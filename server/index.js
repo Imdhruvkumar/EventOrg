c52eccfb85d86cbe224e97d663e8bb0a8a914e17
imdhruvkumar <dk0495682@gmail.com>
2026-07-10 22:13:35 +0530

    Initializing server port and database connection

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require('dotenv');
+const connectDB= require('./config/db.js')
+dotenv.config();
+
+const app = express();
+app.use(cors());
+connectDB();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+});
