b7933f6ecfd83f3cf547ef2e01515a74b717716b
imdhruvkumar <dk0495682@gmail.com>
2026-07-30 21:16:16 +0530

    All pages are created now ready to testing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,8 @@ import Login from './pages/Login';
 import EventDetail from './pages/EventDetails';
 import UserDashboard from './pages/UserDashboard';
 import AdminDashboard from './pages/AdminDashboard';
+import PaymentSuccess from './pages/PaymentSucess';
+import PaymentFailed from './pages/PaymentFailed';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -21,6 +23,8 @@ function App() {
         <Route path="/events/:id" element={<EventDetail />} />
         <Route path="/dashboard" element={<UserDashboard />} />
         <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/payment-success" element={<PaymentSuccess />} />
+        <Route path="/payment-failed" element={<PaymentFailed />} />
       </Routes>
     </Router>
   )
--- client/src/pages/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { AuthContext } from '../context/AuthContext';
+import { AuthContext } from '../context/authContext';
 import api from '../utils/axios';
 import { useNavigate } from 'react-router-dom';
 
